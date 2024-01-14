0c000f1f4b415c58595a37c81e170f4d4616bcb6
ikrama sunasra <ikrama.s@somaiya.edu>
2024-01-14 19:38:12 +0530

    firebase data post

--- frontend/src/pages/driver/driverBoard.js
+++ frontend/src/pages/driver/driverBoard.js
@@ -9,7 +9,7 @@ import BookingForm from "../../components/mapForm";
 import { withRouter } from "react-router-dom";
 import DriverJobs from "../../components/driverJobs";
 import VehicleCards from "../../components/vehicleCards";
-import {useEffect} from 'react'; 
+import { useEffect } from 'react';
 
 import "./driverBoard.css";
 
@@ -55,6 +55,7 @@ function DriverBoard() {
   }
 
   const getUserLocation = () => {
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -64,25 +65,52 @@ function DriverBoard() {
           console.log('User Location:');
           console.log(`Latitude: ${latitude}`);
           console.log(`Longitude: ${longitude}`);
-          console.log("Message being sent"); 
+          console.log("Message being sent");
+          
+          if (latitude && longitude) {
+            fetch('https://driverbazar-543a6-default-rtdb.asia-southeast1.firebasedatabase.app/location.json', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ latitude, longitude }),
+            })
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+              })
+              .catch(error => {
+                console.error('Error sending location data to Python server:', error.message);
+              });
+          }
+
         },
         (error) => {
           console.error('Error getting user location:', error.message);
         }
       );
+
     } else {
       console.error('Geolocation is not supported by this browser.');
     }
-    
+  };
+
+  const setupNextInterval = () => {
+    setTimeout(() => {
+      getUserLocation();
+      setupNextInterval(); // Call the function recursively after 20 seconds
+    }, 60000);
   };
 
   useEffect(() => {
     getUserLocation();
+    setupNextInterval();
+
   }, []);
 
   return (
     <Grid container direction="column" spacing={3} justifyContent="center">
-      
+
       <Grid
         container
         item
@@ -110,7 +138,7 @@ function DriverBoard() {
         <Divider />
       </Grid>
       <Grid item md={9}>
-  
+
         {/* Vehicle Page */}
         <TabPanel value={value} index={0}>
           <VehicleCards />
@@ -124,7 +152,7 @@ function DriverBoard() {
             </Grid>
           </Grid>
         </TabPanel>
-        
+
       </Grid>
     </Grid>
   );
